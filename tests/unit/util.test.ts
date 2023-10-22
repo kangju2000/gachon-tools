c631f3a6cf1da8f70452b2f34e096f3fa6b49868
kang <kangju2000@naver.com>
2023-10-22 19:51:48 +0900

    test: util 테스트코드 수정

--- tests/unit/util.test.ts
+++ tests/unit/util.test.ts
@@ -75,47 +75,38 @@ describe('pipe 테스트', () => {
   })
 })
 
-describe('timeFormat 테스트', () => {
-  beforeAll(() => {
-    const time = new Date('2023-01-01T00:00:00Z')
-    jest.useFakeTimers({})
-    jest.setSystemTime(time)
-  })
-  afterAll(() => {
-    jest.useRealTimers()
-  })
+describe('convertDateTime 테스트', () => {
+  test('날짜를 올바르게 변환한다.', () => {
+    const dateTime = '2022-03-15 23:59:59'
+    expect(convertDateTime(dateTime)).toBe('03월 15일 (화) 23시 59분')
 
-  describe('convertDateTime 테스트', () => {
-    test('날짜를 올바르게 변환한다.', () => {
-      const dateTime = '2022-03-15 23:59:59'
-      expect(convertDateTime(dateTime)).toBe('03월 15일 (화) 23시 59분')
-
-      const dateTime2 = '2023-05-21 23:59:59'
-      expect(convertDateTime(dateTime2)).toBe('05월 21일 (일) 23시 59분')
-    })
-
-    test('날짜가 없으면 빈 문자열을 반환한다.', () => {
-      const dateTime = ''
-      expect(convertDateTime(dateTime)).toBe('')
-    })
+    const dateTime2 = '2023-05-21 23:59:59'
+    expect(convertDateTime(dateTime2)).toBe('05월 21일 (일) 23시 59분')
   })
 
-  describe('allProgress 테스트', () => {
-    test('프로미스 배열을 받아서 프로미스 배열을 반환한다.', async () => {
-      const fakePrmoise = (time: number) =>
-        new Promise(resolve => {
-          setTimeout(() => {
-            resolve(time)
-          }, time)
-        })
-      const promise = [fakePrmoise(1000), fakePrmoise(2000)]
-      const callback = jest.fn()
-      const a = await allProgress(promise, callback)
-      expect(a).toEqual([1000, 2000])
-      expect(callback).toBeCalledTimes(3)
-      ;[0, 50, 100].forEach(progress => {
-        expect(callback).toBeCalledWith(progress)
+  test('날짜가 없으면 빈 문자열을 반환한다.', () => {
+    const dateTime = ''
+    expect(convertDateTime(dateTime)).toBe('')
+  })
+})
+
+describe('allProgress 테스트', () => {
+  test('프로미스 배열을 받아서 프로미스 배열을 반환한다.', async () => {
+    const fakePrmoise = (time: number) =>
+      new Promise(resolve => {
+        setTimeout(() => {
+          resolve(time)
+        }, time)
       })
+    const promise = [fakePrmoise(1000), fakePrmoise(2000)]
+    const callback = jest.fn()
+    const a = await allProgress(promise, callback)
+    expect(a).toEqual([1000, 2000])
+    expect(callback).toBeCalledTimes(3)
+
+    const array = [0, 50, 100]
+    array.forEach(progress => {
+      expect(callback).toBeCalledWith(progress)
     })
   })
 })
