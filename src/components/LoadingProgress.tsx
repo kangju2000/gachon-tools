cdf066779fd337bd5eddbc3e60f70ba57a2eaaa9
kang <kangju2000@naver.com>
2023-10-22 23:12:24 +0900

    style: 로딩 UI 변경

--- src/components/LoadingProgress.tsx
+++ src/components/LoadingProgress.tsx
@@ -1,4 +1,4 @@
-import { Box, Flex, Progress, Text } from '@chakra-ui/react'
+import { Center, CircularProgress, CircularProgressLabel } from '@chakra-ui/react'
 
 type Props = {
   pos: number
@@ -6,26 +6,11 @@ type Props = {
 
 const LoadingProgress = ({ pos }: Props) => {
   return (
-    <Box w="100%" mt="100px">
-      <Flex justify="space-between" mb="4px">
-        <Text fontSize="12px" _light={{ color: 'gray.500' }} _dark={{ color: 'gray.400' }}>
-          과제를 불러오는 중입니다.
-        </Text>
-        <Text fontSize="12px" _light={{ color: 'gray.500' }} _dark={{ color: 'gray.400' }}>
-          {pos}%
-        </Text>
-      </Flex>
-      <Progress
-        value={pos}
-        borderRadius="16px"
-        sx={{
-          '& > div:first-of-type': {
-            transitionProperty: 'width',
-          },
-        }}
-        hasStripe
-      />
-    </Box>
+    <Center h="300px">
+      <CircularProgress value={pos} color="#2F6EA2" display="flex" alignItems="center">
+        <CircularProgressLabel>{pos}%</CircularProgressLabel>
+      </CircularProgress>
+    </Center>
   )
 }
 
