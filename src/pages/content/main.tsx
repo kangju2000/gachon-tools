304a2b3f7ec9b42509df701c03668b661ff9f706
kang <kangju2000@naver.com>
2023-10-27 15:51:12 +0900

    chore: sentry 제거, chakra 피어디펜던시 추가, 디펜던시 버전 업데이트

--- src/pages/content/main.tsx
+++ src/pages/content/main.tsx
@@ -1,26 +1,8 @@
 import { ChakraProvider, extendTheme, type ThemeConfig } from '@chakra-ui/react'
 import { mode, type StyleFunctionProps } from '@chakra-ui/theme-tools'
-import * as Sentry from '@sentry/react'
 import { createRoot } from 'react-dom/client'
 
-import packageJson from './../../../package.json'
-
 import App from '@/pages/content/App'
-
-const { version } = packageJson
-
-Sentry.init({
-  dsn: import.meta.env.VITE_APP_SENTRY_DSN,
-  environment: import.meta.env.MODE,
-  release: version,
-  integrations: [
-    new Sentry.Integrations.Breadcrumbs({ console: true }),
-    new Sentry.BrowserTracing(),
-  ],
-  tracesSampleRate: 1.0,
-  replaysSessionSampleRate: 0.1,
-  replaysOnErrorSampleRate: 1.0,
-})
 
 const root = document.createElement('div')
 root.id = 'root'
@@ -59,9 +41,7 @@ const theme = extendTheme({
 })
 
 createRoot(root).render(
-  <>
-    <ChakraProvider resetCSS={false} theme={theme}>
-      <App />
-    </ChakraProvider>
-  </>,
+  <ChakraProvider resetCSS={false} theme={theme}>
+    <App />
+  </ChakraProvider>,
 )
