649fda05ca9d3a8c6a756f548998be30efa38b74
kang <kangju2000@naver.com>
2023-11-27 01:08:07 +0900

    feat: Popup에 노션 링크 추가

--- src/pages/popup/Popup.tsx
+++ src/pages/popup/Popup.tsx
@@ -1,6 +1,6 @@
-import { Center, Flex, Text, Tooltip } from '@chakra-ui/react'
+import { Center, Flex, Link, Text, Tooltip } from '@chakra-ui/react'
 
-import { FeedbackIcon, GachonLogoIcon, GithubIcon } from '@/components/Icons'
+import { FeedbackIcon, GachonLogoIcon, GithubIcon, NotionIcon } from '@/components/Icons'
 
 function App() {
   return (
@@ -11,20 +11,29 @@ function App() {
       </Text>
       <Text fontSize="14px">가천대학교 사이버캠퍼스 확장 프로그램</Text>
       <div className="flex-grow"></div>
-      <Flex gap="5px">
+      <Flex gap="5px" mt="10px">
         <Tooltip label="깃허브">
-          <a
+          <Link
             href="https://www.github.com/kangju2000/gachon-extension"
             target="_blank"
             rel="noreferrer"
           >
             <GithubIcon cursor="pointer" />
-          </a>
+          </Link>
         </Tooltip>
         <Tooltip label="피드백">
-          <a href="https://forms.gle/uM8M6ghS2ABme5s5A" target="_blank" rel="noreferrer">
-            <FeedbackIcon cursor="pointer" bg="red.300" />
-          </a>
+          <Link href="https://forms.gle/uM8M6ghS2ABme5s5A" target="_blank" rel="noreferrer">
+            <FeedbackIcon cursor="pointer" />
+          </Link>
+        </Tooltip>
+        <Tooltip label="노션">
+          <Link
+            href="https://kangju2000.notion.site/Gachon-Tools-f01d077db229434abfce605c2d26f682?pvs=4"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <NotionIcon cursor="pointer" />
+          </Link>
         </Tooltip>
       </Flex>
     </Center>
--- src/pages/popup/index.tsx
+++ src/pages/popup/index.tsx
@@ -1,5 +1,10 @@
+import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
 
 import Popup from '@/pages/popup/Popup'
 
-createRoot(document.getElementById('app-container') as HTMLElement).render(<Popup />)
+createRoot(document.getElementById('app-container') as HTMLElement).render(
+  <ChakraProvider>
+    <Popup />
+  </ChakraProvider>,
+)
